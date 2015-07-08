32d7a4b707e9ad3e57304c2487a7600cbc9c2354
rlingineni <rlingineni@live.com>
2015-07-07 21:07:22 -0500

    Update to latest Android Syntax

--- Droid/WebRenderer.cs
+++ Droid/WebRenderer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Login;
 using Xamarin.Forms.Platform.Android;
 using Xamarin.Forms;
@@ -8,24 +8,25 @@ using System.IO;
 using Login.Droid;
 using Android.App;
 
+
 [assembly: ExportRenderer (typeof (LoginPage), typeof (LoginPageRenderer))]
 [assembly: Dependency (typeof (Login.Droid.LoginPageRenderer.SaveAndLoad))]
 namespace Login.Droid
 {
 	public class LoginPageRenderer : PageRenderer
 	{
-		protected  void OnModelChanged (VisualElement oldModel, VisualElement newModel)
+		protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
 		{
-			//base.OnModelChanged (oldModel, newModel);
-
+			base.OnElementChanged(e);
+		
 			// this is a ViewGroup - so should be able to load an AXML file and FindView<>
 			var activity = this.Context as Activity;
 
 			var auth = new OAuth2Authenticator (
-				clientId: "", // your OAuth2 client id
+				clientId: "932769670123274", // your OAuth2 client id
 				scope: "", // the scopes for the particular API you're accessing, delimited by "+" symbols
-				authorizeUrl: new Uri (""), // the auth URL for the service
-				redirectUrl: new Uri ("")); // the redirect URL for the service
+				authorizeUrl: new Uri ("https://m.facebook.com/dialog/oauth/"), // the auth URL for the service
+				redirectUrl: new Uri ("https://tripauth.azure-mobile.net/signin-facebook")); // the redirect URL for the service
 
 			auth.Completed += (sender, eventArgs) => {
 				// We presented the UI, so it's up to us to dimiss it on iOS.
